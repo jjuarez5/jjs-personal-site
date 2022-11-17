70171d44eba69c708a73ad8f62e5e873fff27fe1
jjuarez5 <julio.juarez5@outlook.com>
2022-11-17 02:09:59 -0600

    import contact to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import About from "./About";
 import Resume from "./Resume";
+import Contact from "./Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <About />
       <Resume />
+      <Contact />
     </div>
   );
 }
