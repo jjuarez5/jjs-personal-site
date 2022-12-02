c33bbc35da9482d82323c600bced4780655bfc8e
jjuarez5 <julio.juarez5@outlook.com>
2022-12-01 23:03:06 -0600

    import portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./Header";
 import About from "./About";
 import Resume from "./Resume";
 import Contact from "./Contact";
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Resume />
+      <Portfolio />
       <Contact />
     </div>
   );
