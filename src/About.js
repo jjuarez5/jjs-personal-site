fcd3fd9b91da262391ae5afdcd9b93bde0f98593
jjuarez5 <julio.juarez5@outlook.com>
2022-12-01 21:54:27 -0600

    add margin to top and bottom

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ function About() {
   return (
     <div className="bg-blue-800 flex flex-row" id="home">
       <div className="flex flex-col mr-10 mx-10">
-        <div className="flex flex-row mr-20 mr-0.5 mb-5">
+        <div className="flex flex-row mr-20 mr-0.5 mb-5 mt-5">
           <div className="text-2xl text-zinc-100">
             <h2>Hello,</h2>
           </div>
@@ -32,16 +32,17 @@ function About() {
               </a>
             </h2>
           </div>
-          <div className="flex flex-col py-1 text-zinc-100 text-justify">
-            <p>
-              I have {calculateExperience()} years of experience as a software
-              engineer, primarily as a back-end developer. Over the past four
-              years I have created and maintained cloud-hosted web
-              services/api's that have been consumed by hundreds of other
-              developers. I have experience writing technical documentation,
-              implementing new features, and onboarding users to to the api's.
-              In my off time, I like to enhance my knowledge of software
-              engineering, front-end development skills, as well as mentor
+          <div className="flex flex-row py-1 mb-5 text-zinc-100 text-justify">
+            <p className="w-6/12">
+              I have {calculateExperience()} years of experience in the software
+              engineering industry, primarily as a back-end web developer. Over
+              the past four years, I have created and maintained cloud-hosted
+              web services/api's that have been consumed by hundreds of other
+              developers and used by thousands of users. I have experience
+              writing technical documentation, implementing new features, and
+              onboarding users to to the api's. In my off time, I like to
+              enhance my knowledge of software engineering, front-end web
+              development skills, as well as volunteer as a technical mentor to
               veterans and others who are interested in breaking into the tech
               industry.
             </p>
